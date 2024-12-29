f88fa4e8dd0935edc22c982e78c61945727b4d3f
imspdr <konglsh@naver.com>
2024-12-29 19:38:50 +0900

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -144,7 +144,7 @@ module.exports = (env) => {
             test: /\.(woff)$/,
             type: "asset/resource",
             generator: {
-              filename: "[hash][ext]",
+              filename: "[name][ext]",
             },
           },
         ],
