4fe5c446505ffb000b71edc74fb2d85c77890f55
imspdr <konglsh@naver.com>
2024-12-29 19:56:20 +0900

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
       output: {
         path: path.join(__dirname, "docs"),
         filename: "main.js",
-        publicPath: "/algovis",
+        publicPath: "/algovis/",
       },
       resolve: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
@@ -82,7 +82,7 @@ module.exports = (env) => {
       output: {
         path: path.join(__dirname, "docs"),
         filename: "main.js",
-        publicPath: "/algovis",
+        publicPath: "/algovis/",
       },
       resolve: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
